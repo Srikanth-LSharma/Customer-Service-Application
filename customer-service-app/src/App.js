8a667308788c01bac68247c92aa67fd3e51ee782
Srikanth <srikanth.lakshminarayanan@psiog.com>
2021-04-03 18:38:16 +0530

    removedUnncessaryLines

--- customer-service-app/src/App.js
+++ customer-service-app/src/App.js
@@ -1,17 +1,11 @@
 
 import React from 'react';
 import './App.css';
-import AppRouter from '../src/AppRouter'  
-//import Employeelist from './components/employeeList';
-//import Login from '../src/components/login'
+import AppRouter from '../src/AppRouter'
 
 function App() {
   return (
     <div className="App">
-      {/*<Employeelist name="Srikanth" salary="35150"/>
-      <Employeelist name="Hari" salary="55150"/>
-      <Employeelist name= "Vishnu" salary ="29999"> My hobbies are playing cricket </Employeelist>
-  */}
     <AppRouter/>
     </div>
   );
