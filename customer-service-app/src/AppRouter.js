3661fb0dc4b6807e49325b42933d5486eb655621
Srikanth <srikanth.lakshminarayanan@psiog.com>
2021-04-03 18:40:57 +0530

    addedAppRouterfile

---
+++ customer-service-app/src/AppRouter.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
+const AppRouter = () => {
+    return(
+        <Router>      
+                <Switch>  
+                        
+                </Switch>  
+        </Router>  
+    )
+}
+
+export default AppRouter;
