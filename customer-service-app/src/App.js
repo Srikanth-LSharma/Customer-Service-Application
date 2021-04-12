3aeec9621de517fe95bdc79ffcdb881fea77a9e4
Srikanth <srikanth.lakshminarayanan@psiog.com>
2021-04-12 15:22:47 +0530

    test_commit

--- customer-service-app/src/App.js
+++ customer-service-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import AppRouter from '../src/AppRouter'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 
 function App() {
   return (
